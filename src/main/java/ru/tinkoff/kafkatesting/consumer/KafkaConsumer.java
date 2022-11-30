8be1d92ff23307f551d5b3268ce5fd216573309a
extensaman <verus.wedding@gmail.com>
2022-11-30 22:29:19 +0300

    feat: configure Producer and refactor sendMessage-method

--- src/main/java/ru/tinkoff/kafkatesting/consumer/KafkaConsumer.java
+++ src/main/java/ru/tinkoff/kafkatesting/consumer/KafkaConsumer.java
@@ -7,7 +7,7 @@ import ru.tinkoff.kafkatesting.controller.MessageController;
 @EnableKafka
 public class KafkaConsumer {
 
-    @KafkaListener(topics = MessageController.TOPIC_NAME)
+    @KafkaListener(topics = MessageController.TOPIC_NAME, groupId = "lopata")
     public void listenMessage(String message){
         System.out.println(message);
     }
--- src/main/java/ru/tinkoff/kafkatesting/controller/MessageController.java
+++ src/main/java/ru/tinkoff/kafkatesting/controller/MessageController.java
@@ -1,22 +1,28 @@
 package ru.tinkoff.kafkatesting.controller;
 
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("msg")
 public class MessageController {
     public static final String TOPIC_NAME = "msg";
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<Long, String> kafkaTemplate;
 
-    public MessageController(KafkaTemplate<String, String> kafkaTemplate) {
+    public MessageController(KafkaTemplate<Long, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     @PostMapping
-    public void sendMessage(String id, String message) {
-        kafkaTemplate.send(TOPIC_NAME, id, message);
+    public void sendMessage(Long id, String message) {
+        CompletableFuture<SendResult<Long, String>> future = kafkaTemplate.send(TOPIC_NAME, id, message);
+        future.thenAccept(System.out::println);
+        kafkaTemplate.flush();
     }
 }
